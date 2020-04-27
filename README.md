d0638e7710fb1f61709ae32fc998f7d9df84493f
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-04-27 22:36:21 +0530

    Root Commit
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Twitch Stream Viewer
+
+This is currently targeted at **Valorant Drops Enabled** streams, Valorant is in closed beta, as of writing this. It can be obtained by watching __Drops Enabled__ twitch streams. But what if you cant watch them?
+
+Then, my tool comes to rescue!
+
+- Just install `xvfb and google-chrome` from your favourite package manager.
+
+- Run `pip install selenium xvfbwrapper`
+
+- Run the associated python file, twitch.py, boom!
---
+++ twitch.py
@@ -0,0 +1,52 @@
+#
+# Copyright © 2019 Maestro Creativescape
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+from time import sleep
+from os import environ
+from selenium.webdriver.common.action_chains import ActionChains
+from xvfbwrapper import Xvfb
+
+try:
+    vdisplay = Xvfb()
+    vdisplay.start()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(30)
+
+    driver.get("http://www.twitch.tv/login")
+    username_input = driver.find_element_by_xpath("//div[@data-a-target='login-username-input']/input")
+    password_input = driver.find_element_by_xpath("//div[@data-a-target='login-password-input']/input")
+    login_button = driver.find_element_by_xpath("//button[@data-a-target='passport-login-button']")
+    username_input.clear()
+    username_input.send_keys(environ.get("TWITCH_USERNAME"))
+    password_input.clear()
+    password_input.send_keys(environ.get("TWITCH_PASSWORD"))
+    login_button.click()
+    two_step = driver.find_element_by_xpath("//input[@data-a-target='tw-input']")
+    two_step_key = input("Enter the two step key to login: ")
+    two_step = driver.find_element_by_xpath("//input[@data-a-target='tw-input']")
+    two_step.send_keys(two_step_key)
+    two_step.send_keys(Keys.RETURN)
+    while(True):
+        driver.get("https://twitch.tv")
+        search_box = driver.find_element_by_xpath("//div[@data-a-target='tray-search-input']/input")
+        search_box.send_keys("Valorant Drops Enabled")
+        search_box.send_keys(Keys.RETURN)
+        first_channel = driver.find_element_by_xpath("//div[@data-a-target='search-result-live-channel']/a")
+        first_channel.click()
+        sleep(60*60*2.1) #Sleep for 2.1hrs
+
+except:
+    driver.quit()
+    vdisplay.stop()
